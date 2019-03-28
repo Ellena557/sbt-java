f767d7951c2d7bc6075b7a479e173f1d003bb664
Elena <helena557@mail.ru>
2019-03-28 14:38:36 +0300

    structure changed

---
+++ hw-2/tests/AnotherPerson.java
@@ -0,0 +1,36 @@
+public class AnotherPerson {
+    public String name;
+    private int age;
+    private String profession;
+
+    /*public AnotherPerson(String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+    */
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+}
