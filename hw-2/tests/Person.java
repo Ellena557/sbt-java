4aa40d0e850002d07ddce8999370e2bff3523c00
Elena Gorskaya <helena557@mail.ru>
2019-03-28 14:39:22 +0300

    Update Person.java

--- hw-2/tests/Person.java
+++ hw-2/tests/Person.java
@@ -3,13 +3,6 @@ public class Person {
     private int age;
     private String hairColor;
 
-    /*public Person(String name, int age, String hairColor) {
-        this.name = name;
-        this.age = age;
-        this.hairColor = hairColor;
-    }
-    */
-
     public int getAge() {
         return age;
     }
