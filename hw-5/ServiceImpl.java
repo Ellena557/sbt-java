5692bc7b11f4a742537a6b1c6db1739eb6cb7a8f
Elena Gorskaya <helena557@mail.ru>
2019-05-17 14:29:14 +0300

    Update ServiceImpl.java

--- hw-5/ServiceImpl.java
+++ hw-5/ServiceImpl.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class ServiceImpl implements Service {
     @Override
     @Cache(identityBy = String.class)
@@ -5,5 +8,25 @@ public class ServiceImpl implements Service {
         System.out.println(s + " " + a);
         double r = a;
         return r;
+    }
+
+    @Override
+    @Cache(identityBy = String.class, keyName = "work")
+    public double doLessHardWork(String s, int a) {
+        System.out.println(s + " " + a);
+        double r = a;
+        return r;
+    }
+
+    @Override
+    @Cache(identityBy = String.class, keyName = "list", listList = 2)
+    public List<Integer> doListWork(String s, int a) {
+        System.out.println(s + " " + a);
+        List<Integer> cacheList = new LinkedList<>();
+        cacheList.add(a);
+        cacheList.add(a);
+        cacheList.add(a);
+        cacheList.add(a);
+        return cacheList;
     }
 }
