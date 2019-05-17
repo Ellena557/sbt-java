18817972dd8ca211c07582f1e7d444a69801fecf
Elena Gorskaya <helena557@mail.ru>
2019-05-17 14:28:18 +0300

    Update Cache.java

--- hw-5/Cache.java
+++ hw-5/Cache.java
@@ -3,11 +3,13 @@ import java.lang.annotation.*;
 @Documented
 //Аннотация может быть использована во время выполнения через Reflection
 @Retention(RetentionPolicy.RUNTIME)
-//Аннотации  направлена именно на метод (не класс, не переменная, не поле, а именно метод)
+//Аннотация  направлена именно на метод (не класс, не переменная, не поле, а именно метод)
 @Target(ElementType.METHOD)
 public @interface Cache {
-    //CacheType hashType() default CacheType.IN_MEMORY;
+    CacheType cacheType() default CacheType.IN_MEMORY;
     //String fileNamePrefix();
     boolean zip() default false;
     Class[] identityBy();
+    String keyName() default "";
+    int listList() default Integer.MAX_VALUE;
 }
