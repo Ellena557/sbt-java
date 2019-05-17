7bf92519a59fa778b044e91add64ccc4cd7478dd
Elena Gorskaya <helena557@mail.ru>
2019-05-17 14:28:57 +0300

    Update Service.java

--- hw-5/Service.java
+++ hw-5/Service.java
@@ -1,4 +1,12 @@
+import java.util.List;
+
 public interface Service {
     @Cache(identityBy = String.class)
     double doHardWork(String s, int a);
+
+    @Cache(identityBy = String.class, keyName = "work")
+    double doLessHardWork(String s, int a);
+
+    @Cache(identityBy = String.class, keyName = "list", listList = 2)
+    List<Integer> doListWork(String s, int a);
 }
