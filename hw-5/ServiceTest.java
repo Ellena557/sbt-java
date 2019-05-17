019ffc2358da032f3b0db502c89d3bb6bc76ef61
Elena Gorskaya <helena557@mail.ru>
2019-05-17 14:27:57 +0300

    Update ServiceTest.java

--- hw-5/ServiceTest.java
+++ hw-5/ServiceTest.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class ServiceTest {
     public static void main(String[] args) {
         ServiceImpl service = new ServiceImpl();
@@ -5,8 +7,23 @@ public class ServiceTest {
 
         Service service1 = (Service) CacheProxy.cacheObject(service);
 
+        System.out.println("First test : choosing parameters");
+
         double r1 = service1.doHardWork("work1", 10); //считает результат
         double r2 = service1.doHardWork("work1", 5);
+
+        System.out.println();
+        System.out.println("Next test : keyName");
+
+        double r3 = service1.doLessHardWork("work1", 10); //считает результат
+        double r4 = service1.doLessHardWork("work1", 5);
+
+        System.out.println();
+        System.out.println("Next test : List");
+
+        List<Integer> r5 = service1.doListWork("list1", 10); //считает результат
+        List<Integer> r6 = service1.doListWork("list1", 5);
+        System.out.println(r5.size() + " " + r6.size());
     }
 
     static void run(ServiceImpl service) {
@@ -14,7 +31,4 @@ public class ServiceTest {
         double r2 = service.doHardWork("work2", 5);  //считает результат
         double r3 = service.doHardWork("work1", 10); //результат из кеша
     }
-
-
-
 }
