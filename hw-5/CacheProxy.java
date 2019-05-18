543af93510b97942213e1b9eacc2aa6a0a7fe609
Elena Gorskaya <helena557@mail.ru>
2019-05-18 03:39:52 +0300

    Update CacheProxy.java

--- hw-5/CacheProxy.java
+++ hw-5/CacheProxy.java
@@ -1,10 +1,13 @@
+import java.io.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-public class CacheProxy implements InvocationHandler {
+public class CacheProxy implements InvocationHandler, Serializable {
 
     private Map cacheMap;
     private Object[] args;
@@ -49,13 +52,14 @@ public class CacheProxy implements InvocationHandler {
 
         Cache cacheAnnotation = method.getAnnotation(Cache.class);
         Class[] identities = cacheAnnotation.identityBy();
+        //System.out.println(identities);
         String nameOfKey = cacheAnnotation.keyName();
+        CacheType cachType = cacheAnnotation.cacheType();
+        boolean zip = cacheAnnotation.zip();
 
         if (nameOfKey == ""){
             nameOfKey = method.getName();
         }
-
-        Map cache = getCacheValue(nameOfKey);
 
         List mentionedParams = new ArrayList();
 
@@ -66,6 +70,7 @@ public class CacheProxy implements InvocationHandler {
             for (int i = 0; i < identities.length; i++) {
                 if (identities[i] == param.getClass()) {
                     isMentioned = true;
+                    //System.out.println(identities[i] + " III");
                 }
             }
 
@@ -74,10 +79,51 @@ public class CacheProxy implements InvocationHandler {
             }
         }
 
+        String fileName = nameOfKey + ".txt";
+        String zipFileName = nameOfKey + ".zip";
+        Object vals = null;
+        Map cache = null;
+
+        //Class[] defaultClass = new Class[1];
+        //defaultClass[0] = Class.class;
+
+        // if we have default identities
+        if (identities[0] == Class.class) {
+            mentionedParams = params;
+        }
+
+        if (cachType == CacheType.IN_MEMORY) {
+            cache = getCacheValue(nameOfKey);
+            vals = cache.get(mentionedParams);
+
+        } else {
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+                cache = (HashMap<Object,Object>)ois.readObject();
+
+                vals = cache.get(mentionedParams);
+                ois.close();
+                /*String mentionedParamsStr = "";
+                for (Object param : mentionedParams) {
+
+                }
+                ArrayList<ArrayList<Object>> cache = (ArrayList<ArrayList<Object>>) ois.readObject();
+                for (ArrayList<Object> obj : cache) {
+                    if
+                }*/
+            } catch (FileNotFoundException e) {
+                File file = new File(fileName);
+                cache = new HashMap();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+
         //System.out.println(mentionedParams + "PPPPPP");
 
-
-        Object vals = cache.get(mentionedParams);
         //System.out.println(vals.getClass());
 
 
@@ -110,9 +156,32 @@ public class CacheProxy implements InvocationHandler {
                 if (valsList.size() > maxListSize) {
                     vals = valsList.subList(0, maxListSize);
                 }
+
             }
 
             cache.put(mentionedParams, vals);
+
+            if (cachType == CacheType.FILE) {
+                try {
+                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName));
+                    FileInputStream fis = new FileInputStream(fileName);
+                    oos.writeObject(cache);
+                    oos.flush();
+
+                    if (zip) {
+                        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFileName));
+                        ZipEntry entry = new ZipEntry(fileName);
+                        zos.putNextEntry(entry);
+                        byte[] byteCode = new byte[fis.available()];
+                        fis.read(byteCode);
+                        zos.write(byteCode);
+                        zos.closeEntry();
+                        zos.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             //for (Object param : mentionedParams){
               //  System.out.println(param.getClass());
             //}
@@ -127,7 +196,6 @@ public class CacheProxy implements InvocationHandler {
         }*/
         return vals;
     }
-
 
 
     private Map getCacheValue(String method){
