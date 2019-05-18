bfe17527c8c6b55d062feea064cbb9e9a5426037
Elena Gorskaya <helena557@mail.ru>
2019-05-18 14:36:51 +0300

    refactoring

--- hw-5/ServiceTest.java
+++ hw-5/ServiceTest.java
@@ -9,38 +9,38 @@ public class ServiceTest {
 
         System.out.println("First test : choosing parameters");
 
-        double r1 = service1.doHardWork("work1", 10); //считает результат
+        double r1 = service1.doHardWork("work1", 10);
         double r2 = service1.doHardWork("work1", 5);
 
         System.out.println();
         System.out.println("Next test : keyName");
 
-        double r3 = service1.doLessHardWork("work1", 10); //считает результат
+        double r3 = service1.doLessHardWork("work1", 10);
         double r4 = service1.doLessHardWork("work1", 5);
 
         System.out.println();
         System.out.println("Next test : List");
 
-        List<Integer> r5 = service1.doListWork("list1", 10); //считает результат
+        List<Integer> r5 = service1.doListWork("list1", 10);
         List<Integer> r6 = service1.doListWork("list1", 5);
         System.out.println(r5.size() + " " + r6.size());
 
         System.out.println();
         System.out.println("Next test : File");
 
-        double r7 = service1.doFileWork("work1", 10); //считает результат
+        double r7 = service1.doFileWork("work1", 10);
         double r8 = service1.doFileWork("work1", 5);
 
         System.out.println();
         System.out.println("Next test : Zip");
 
-        double r9 = service1.doZipWork("work1", 10); //считает результат
+        double r9 = service1.doZipWork("work1", 10);
         double r10 = service1.doZipWork("work1", 5);
 
         System.out.println();
         System.out.println("Next test : Default parameters");
 
-        double r11 = service1.doParameterWork("work1", 10); //считает результат
+        double r11 = service1.doParameterWork("work1", 10);
         double r12 = service1.doParameterWork("work1", 5);
     }
 
