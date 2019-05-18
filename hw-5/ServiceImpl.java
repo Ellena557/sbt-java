9fd99fb08115adba02baf49a4e024b416895325c
Elena Gorskaya <helena557@mail.ru>
2019-05-18 03:40:30 +0300

    Update ServiceImpl.java

--- hw-5/ServiceImpl.java
+++ hw-5/ServiceImpl.java
@@ -29,4 +29,28 @@ public class ServiceImpl implements Service {
         cacheList.add(a);
         return cacheList;
     }
+
+    @Override
+    @Cache(identityBy = String.class, keyName = "file", cacheType = CacheType.FILE)
+    public double doFileWork(String s, int a) {
+        //System.out.println(s + " " + a);
+        double r = a;
+        return r;
+    }
+
+    @Override
+    @Cache(identityBy = String.class, keyName = "zip", cacheType = CacheType.FILE, zip = true)
+    public double doZipWork(String s, int a) {
+        //System.out.println(s + " " + a);
+        double r = a * 2;
+        return r;
+    }
+
+    @Override
+    @Cache()
+    public double doParameterWork(String s, int a) {
+        System.out.println(s + " " + a);
+        double r = a * 2;
+        return r;
+    }
 }
