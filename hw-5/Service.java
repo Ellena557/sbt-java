581edfdcc8910ff64177c9c2ad3050f5739bdf92
Elena Gorskaya <helena557@mail.ru>
2019-05-18 03:40:10 +0300

    Update Service.java

--- hw-5/Service.java
+++ hw-5/Service.java
@@ -9,4 +9,13 @@ public interface Service {
 
     @Cache(identityBy = String.class, keyName = "list", listList = 2)
     List<Integer> doListWork(String s, int a);
+
+    @Cache(identityBy = String.class, keyName = "file", cacheType = CacheType.FILE)
+    double doFileWork(String s, int a);
+
+    @Cache(identityBy = String.class, keyName = "zip", cacheType = CacheType.FILE, zip = true)
+    double doZipWork(String s, int a);
+
+    @Cache()
+    double doParameterWork(String s, int a);
 }
